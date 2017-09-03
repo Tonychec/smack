35ef5bf131cfebc7045aebc6fa6f1c42635d9e9a
Nomad <nomad@Nomads-MacBook-Pro.local>
2017-09-03 15:03:58 +0300

    End of lesson 4.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nomad on 9/3/17.
+//  Copyright © 2017 ios.Nomad. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+
+let TO_LOGIN = "toLogin"
