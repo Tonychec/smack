464e86e20748742183024d0c45841d977d56a89f
Nomad <nomad@Nomads-MBP.Dlink>
2017-09-09 16:00:27 +0300

    Lesson 11

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 //URL Constants
 let BASE_URL = "https://testchatapi.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN  = "\(BASE_URL)account/login"
 
 // Seques
 let TO_LOGIN = "toLogin"
@@ -24,3 +25,8 @@ let UNWIDE = "unwindeToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Header
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
