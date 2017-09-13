e1395173ce47781f57d3045ad49a4bc21ca23cd5
Nomad <nomad@Nomads-MacBook-Pro.local>
2017-09-13 16:08:56 +0300

    lesson 17

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -21,6 +21,10 @@ class ChannelVC: UIViewController {
     
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupUserInfo()
+    }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         if AuthService.instance.isLoggedIn {
@@ -28,12 +32,17 @@ class ChannelVC: UIViewController {
             let profile = ProfileVC()
             profile.modalPresentationStyle = .custom
             present(profile, animated: true, completion: nil)
+            //.
         } else {
              performSegue(withIdentifier: TO_LOGIN, sender: nil)
         }
     }
     
     @objc func userDataDidChange(_ notif: Notification) {
+        setupUserInfo()
+    }
+    
+    func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
             loginBtn.setTitle(UserDataServece.instance.name, for: .normal)
             userImg.image = UIImage(named: UserDataServece.instance.avatarName)
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = emailTxt.text, emailTxt.text != "" else  { return }
+        guard let pass = passTxt.text, passTxt.text != "" else  { return }
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil) 
+                    }
+                })
+            }
+        }
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
@@ -21,6 +47,12 @@ class LoginVC: UIViewController {
     
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        emailTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
+        passTxt.attributedPlaceholder = NSAttributedString(string: "password ", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
     }
 }
  
